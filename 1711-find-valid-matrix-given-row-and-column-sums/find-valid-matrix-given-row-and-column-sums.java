c7bb4baf4fee19b5308c833f50be8547cfe87f1d
Jayesh Jadhav <128024447+ItzKrishJJ@users.noreply.github.com>
2024-07-21 15:38:22 +0530

    Time: 7 ms (52.38%) | Memory: 53.1 MB (26.59%) - LeetSync

--- 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.java
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.java
@@ -1,24 +1,17 @@
 class Solution {
     public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
         int numrows = rowSum.length;
-        int colrows = colSum.length;
-        int[][] result = new int[numrows][colrows];
-
-        int i=0,j=0;
-
-        while(i<numrows && j<colrows){
-            int val = Math.min(rowSum[i], colSum[j]);
-            result[i][j] = val;
-            rowSum[i] -= val;
-            colSum[j] -= val;
-
-            if(rowSum[i] == 0){
-                i++;
-            }
-            if(colSum[j] == 0){
-                j++;
+        int numcols = colSum.length;  // Fixed the typo here
+        int[][] result = new int[numrows][numcols];
+        
+        for (int i = 0; i < numrows; i++) {
+            for (int j = 0; j < numcols; j++) {  // Reset j in each iteration of the outer loop
+                result[i][j] = Math.min(rowSum[i], colSum[j]);
+                rowSum[i] -= result[i][j];
+                colSum[j] -= result[i][j];
             }
         }
+        
         return result;
     }
 }
