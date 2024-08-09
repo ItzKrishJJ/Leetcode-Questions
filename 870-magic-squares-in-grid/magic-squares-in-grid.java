48e08adbb07f5e37d2365ff705cb3baf8f1b6904
Jayesh Jadhav <128024447+ItzKrishJJ@users.noreply.github.com>
2024-08-09 21:58:20 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (84.86%) - LeetSync

--- 870-magic-squares-in-grid/magic-squares-in-grid.java
+++ 870-magic-squares-in-grid/magic-squares-in-grid.java
@@ -3,7 +3,6 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
         int count = 0; 
-        
         for(int i = 0; i < m - 2; i++) { 
             for(int j = 0; j < n - 2; j++) { 
                 if(isMagicGrid(grid, i, j)) {
@@ -11,7 +10,6 @@ class Solution {
                 }
             } 
         }
-        
         return count;
     }
 
@@ -25,21 +23,17 @@ class Solution {
                 seen[num] = true;
             }
         }
-
         int sum = grid[i][j] + grid[i][j+1] + grid[i][j+2];
-
         for(int x = 0; x < 3; x++) {
             if(sum != grid[i + x][j] + grid[i + x][j+1] + grid[i + x][j+2]) {
                 return false;
             }
         }
-
         for(int y = 0; y < 3; y++) {
             if(sum != grid[i][j + y] + grid[i+1][j + y] + grid[i+2][j + y]) {
                 return false;
             }
         }
-
         // Check diagonals
         if(sum != grid[i][j] + grid[i+1][j+1] + grid[i+2][j+2]) return false;
         if(sum != grid[i+2][j] + grid[i+1][j+1] + grid[i][j+2]) return false; 
