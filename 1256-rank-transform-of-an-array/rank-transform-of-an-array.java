cb393c10c9a0c0a04107d5c4a6d5121369c9426c
Jayesh Jadhav <128024447+ItzKrishJJ@users.noreply.github.com>
2024-10-02 18:35:13 +0530

    Time: 26 ms (69.92%) | Memory: 61.7 MB (70.50%) - LeetSync

---
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int n = arr.length;
+        if (n == 0) {
+            return new int[0];
+        }
+
+        int newarr[] = arr.clone();
+        Arrays.sort(newarr);
+        
+        HashMap<Integer, Integer> mymap = new HashMap<>();
+        int rank = 1;
+        mymap.put(newarr[0], rank);
+        
+        for (int i = 1; i < newarr.length; i++) {
+            if (newarr[i] != newarr[i - 1]) {
+                rank++;
+            }
+            mymap.put(newarr[i], rank);
+        }
+        
+        int rankarr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            rankarr[i] = mymap.get(arr[i]);
+        }
+        
+        return rankarr;
+    }
+}
